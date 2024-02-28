9e89cff282dbd10d4103ae6921eb66960f313b5c
Kishan Singh <kishansingh@Kishans-MacBook-Pro-2.local>
2024-02-28 13:12:57 +0530

    Add font theme using google font pub

--- instagram_clone/lib/Presentation/home_screen.dart
+++ instagram_clone/lib/Presentation/home_screen.dart
@@ -1,67 +1,70 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-const HomeScreen({super.key});
+  const HomeScreen({super.key});
 
-@override
-State<HomeScreen> createState() => _HomeScreenState();
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-int _selectedIndex = 0;
+  int _selectedIndex = 0;
 
-void _onItemTapped(int index) {
-setState(() {
-_selectedIndex = index;
-});
-}
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: Row(
-children: [
-SizedBox(
-width: 8), // Add some spacing between the icon and the title
-Text('Instagram',), // Your title text
-],
-),
-),
-body: Center(
-child: Text('Page ${_selectedIndex + 1}'),
-),
-bottomNavigationBar: BottomNavigationBar(
-iconSize: 32,
-type: BottomNavigationBarType.fixed, // Set type to fixed
-currentIndex: _selectedIndex,
-onTap: _onItemTapped,
-items: const [
-BottomNavigationBarItem(
-icon: Icon(Icons.home),
-label: '',
-),
-BottomNavigationBarItem(
-icon: Icon(
-Icons.search,
-),
-label: '',
-),
-BottomNavigationBarItem(
-icon: Icon(Icons.add_box_rounded),
-label: '',
-),
-BottomNavigationBarItem(
-icon: Icon(Icons.play_arrow),
-label: '',
-),
-BottomNavigationBarItem(
-icon: Icon(Icons.person),
-label: '',
-),
-],
-),
-);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            const SizedBox(
+                width: 8), // Add some spacing between the icon and the title
+            Text(
+              'Instagram',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ), // Your title text
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text('Page ${_selectedIndex + 1}',
+            style: Theme.of(context).textTheme.bodySmall),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 32,
+        type: BottomNavigationBarType.fixed, // Set type to fixed
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box_rounded),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.play_arrow),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
 }
-}
-
--- instagram_clone/lib/main.dart
+++ instagram_clone/lib/main.dart
@@ -8,25 +8,23 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         textTheme: TextTheme(
           //Body Small
-          bodySmall: GoogleFonts.montserrat(
-              fontSize: 13, fontWeight: FontWeight.w600, color: Colors.black),
+          bodyLarge: GoogleFonts.greatVibes(
+              fontSize: 28, fontWeight: FontWeight.w500, color: Colors.black),
 
           //Body Medium
-          bodyMedium: GoogleFonts.montserrat(
-              fontSize: 16, color: Colors.black, fontWeight: FontWeight.w700),
+          bodyMedium: GoogleFonts.outfit(
+              fontSize: 18, fontWeight: FontWeight.w500, color: Colors.black),
 
           //Body Large
-          bodyLarge: GoogleFonts.aBeeZee(
-              fontSize: 19, fontWeight: FontWeight.w800, color: Colors.white),
+          bodySmall: GoogleFonts.outfit(
+              fontSize: 16, fontWeight: FontWeight.w400, color: Colors.black),
         ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
