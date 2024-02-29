f360d759b90dde6d89d5a80b098c0d69ea4c50db
Vijay Pratap Singh <vijayprsingh619@gmail.com>
2024-02-29 16:40:31 +0530

    [Vijay] Add test for feed screen and remove obs from home controller as we are using GetBuilder

---
+++ instagram_clone/test/feed_screen_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:instagram_clone/Controllers/home_controller.dart';
+import 'package:instagram_clone/Presentation/feed_screen.dart';
+import 'package:instagram_clone/Presentation/feed_post.dart';
+
+void main() {
+  testWidgets('FeedScreen builds correctly', (WidgetTester tester) async {
+    // Create a fake instance of HomeController and provide it using Get.put
+    final HomeController _controller = HomeController();
+    Get.put(_controller);
+
+    // Build our widget
+    await tester.pumpWidget(const MaterialApp(home: FeedScreen()));
+
+    // Verify that the title is rendered
+    expect(find.text('Instagram'), findsOneWidget);
+
+    // Verify that the FeedPost widgets are rendered based on the number of items in the feedList
+    expect(find.byType(FeedPost), findsNWidgets(_controller.feedList.length));
+  });
+}
