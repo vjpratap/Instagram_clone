b169877a44fe091db896436f6438f61088c2f16e
Vijay Pratap Singh <vijayprsingh619@gmail.com>
2024-03-01 16:06:04 +0530

    [Vijay] Update feed screen test

--- instagram_clone/test/feed_screen_test.dart
+++ instagram_clone/test/feed_screen_test.dart
@@ -6,10 +6,11 @@ import 'package:instagram_clone/Presentation/feed_screen.dart';
 import 'package:instagram_clone/Presentation/feed_post.dart';
 
 void main() {
-  testWidgets('FeedScreen builds correctly', (WidgetTester tester) async {
-    // Create a fake instance of HomeController and provide it using Get.put
+  testWidgets('FeedScreen builds For loading', (WidgetTester tester) async {
     final HomeController controller = HomeController();
     Get.put(controller);
+
+    controller.state = StatusWithValue(FeedsState.loading, []);
 
     // Build our widget
     await tester.pumpWidget(const MaterialApp(home: FeedScreen()));
@@ -17,7 +18,9 @@ void main() {
     // Verify that the title is rendered
     expect(find.text('Instagram'), findsOneWidget);
 
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
     // Verify that the FeedPost widgets are rendered based on the number of items in the feedList
-    expect(find.byType(FeedPost), findsNWidgets(controller.feedList.length));
+    expect(find.byType(FeedPost), findsNWidgets(controller.state.feeds.length));
   });
 }
