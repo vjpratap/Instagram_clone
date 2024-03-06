d021d60167526cfb1691e87aa5f8f67237291381
Vijay Pratap Singh <vijayprsingh619@gmail.com>
2024-03-06 15:12:41 +0530

    [Vijay] Add dio for networking in home controller

--- instagram_clone/lib/Feeds/controllers/home_controller.dart
+++ instagram_clone/lib/Feeds/controllers/home_controller.dart
@@ -1,17 +1,19 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:instagram_clone/Feeds/Models/feed.dart';
 import 'package:instagram_clone/Utility/url.dart';
+import 'package:dio/dio.dart';
 
-sealed class FeedScreenState{}
+sealed class FeedScreenState {}
+
+final dio = Dio();
 
 class FeedLoadingState extends FeedScreenState {}
+
 class FeedSuccessState extends FeedScreenState {
   final List<Feed> feeds;
   FeedSuccessState(this.feeds);
 }
+
 class FeedFailureState extends FeedScreenState {}
 
 class HomeController extends GetxController {
@@ -24,17 +26,17 @@ class HomeController extends GetxController {
 
   Future<void> fetchData() async {
     try {
-      final response = await http.get(Uri.parse(URL.feedURL));
+      final response = await dio.get(URL.feedURL);
       if (response.statusCode == 200) {
-        final List<dynamic> jsonList = json.decode(response.body);
-        List<Feed> feeds = jsonList.map((json) => Feed.fromJson(json)).toList();
+        List<Feed> feeds =
+            response.data.map<Feed>((data) => Feed.fromJson(data)).toList();
         // feedList = feeds;
         state = FeedSuccessState(feeds);
       } else {
         state = FeedFailureState();
       }
     } catch (e) {
-        state = FeedFailureState();
+      state = FeedFailureState();
     }
     update();
   }
